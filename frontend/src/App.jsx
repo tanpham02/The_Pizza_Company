6cef7204be2965f0d82c89156d3415be0f32ba10
Văn Tân <phamvantan1311@gmail.com>
2023-05-30 10:51:07 +0700

    update logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,12 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev">
-          <img src='/assets/img/products/drink/7up-fiber-lon.png' className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      <img src="/assets/img/logo/logo-main.png" />
+      <h2>
+        The Pizza Company Clone
+      </h2>
+    </div>
   )
 }
 
