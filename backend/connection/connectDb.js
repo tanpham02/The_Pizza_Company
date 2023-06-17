17b47294a6b131f6a9680575ce6eaf330ae1ebfa
Văn Tân <phamvantan1311@gmail.com>
2023-06-17 21:30:42 +0700

    update

--- backend/connection/connectDb.js
+++ backend/connection/connectDb.js
@@ -4,10 +4,13 @@ dotenv.config({ path: '.env' })
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URL_CLOUD, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        const conn = await mongoose.connect(
+            "mongodb+srv://thepizzacompany:thepizzacompany123@cluster0.tqwu0qn.mongodb.net/ThePizzaCompany?retryWrites=true&w=majority",
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        )
         console.log(`Connect to MongoDB success with ${conn.connection.host}`)
     } catch (err) {
         console.log(`Connect MongoDB failure with ${err}`)
