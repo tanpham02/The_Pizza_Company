61f46cb08e1df2cd47115b00cc483ebdd52dbc7e
Văn Tân <phamvantan1311@gmail.com>
2023-06-20 10:08:47 +0700

    update

--- backend/connection/connectDb.js
+++ backend/connection/connectDb.js
@@ -6,7 +6,7 @@ const MONGO_URL_LOCAL = "mongodb://localhost:27017/ThePizzaCompany"
 const connectDb = async () => {
     try {
         const conn = await mongoose.connect(
-            MONGO_URL_LOCAL,
+            MONGO_URL_CLOUD,
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true
